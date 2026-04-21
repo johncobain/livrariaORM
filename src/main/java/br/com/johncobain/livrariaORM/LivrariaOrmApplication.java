061abdd68f5b6b9cd3e354a54daf905f5bc1b2e1
Andrey Gomes <andreygdsn@gmail.com>
2026-04-21 17:16:57 -0300

    initial config

---
+++ src/main/java/br/com/johncobain/livrariaORM/LivrariaOrmApplication.java
@@ -0,0 +1,13 @@
+package br.com.johncobain.livrariaORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/johncobain/livrariaORM/LivrariaOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johncobain.livrariaORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
